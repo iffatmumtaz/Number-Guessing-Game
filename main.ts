ddc8a1535d31da77410f21a7be8f53cb94de3c8b
Iffat mumtaz <160539573+iffatmumtaz@users.noreply.github.com>
2024-06-26 15:12:23 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("Guess a number between 1 to 20");
+  
+
+///Creating variables
+
+let a:number=Math.floor(Math.random()*20);
+
+while(true){
+let input= await inquirer.prompt
+({name:"guess",
+ type:"number",
+ message:"Enter your guessing number:"})
+
+
+//code for given number
+
+let ans:number=input.guess
+ if (a==ans)
+{console.log("congratulations your guessing number is correct")
+break;}
+else{console.log("sorry try again")}
+
+
+//adding hint
+if(a>ans){console.log("hint:number is higher")}
+else {console.log("number is lower")}
+
+console.log("GaMe Is OvEr")
+
+};
